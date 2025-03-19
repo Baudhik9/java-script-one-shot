141b8efdad4d7247eb6c593bf65007669b05eb4d
Baudhik9 <baudhikmadan2005@gmail.com>
2025-03-19 17:10:23 +0000

    converions

---
+++ conversion and operation/conversion.js
@@ -0,0 +1,27 @@
+let score = "33"
+//console.log(typeof score);//used to find the type of variable
+//console.log(typeof(score));//another way of typeof
+
+let value_in_number= Number(score)//converts string or any datatype into the number datatype
+//console.log(typeof value_in_number);
+
+let a ="33abs"
+let val=Number(a)
+//console.log(typeof val);
+
+//console.log(val);//value of a string having alpha can can be change in number but value is NaN(not a number)
+//true=>1
+//null=>0
+
+let logged=1
+let conlogged=Boolean(logged)//number is converted into boolean
+//console.log(typeof conlogged);
+//console.log(conlogged);
+
+//"" => false
+
+let num=12
+let strnum=String(num)//number is converted into string
+console.log(strnum);
+console.log(typeof strnum);
+
