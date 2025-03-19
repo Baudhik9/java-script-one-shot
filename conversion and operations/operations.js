b10ea4c649d63adfe52166e7088ba6878d5967ed
Baudhik9 <baudhikmadan2005@gmail.com>
2025-03-19 17:27:19 +0000

    conversions and operations

---
+++ conversion and operations/operations.js
@@ -0,0 +1,25 @@
+let val=3
+let negval = -val
+//console.log(negval);
+
+let str1="hello"
+let str2=" baudhik"
+let str3=str1+str2
+//console.log(str3);
+
+//console.log(1+2+"2");//output 32
+
+//console.log("1" +2+2);//output 122
+
+//console.log((1+2+2));//output 5
+
+//console.log(+true);//output 1
+
+//console.log(+""); //output 0
+
+let k=32
+k++
+//console.log(k);//output 33
+
+
+
