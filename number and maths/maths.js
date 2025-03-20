14b8ae2acfe60cc4e21e007bfae2e7d1916f5c76
Baudhik9 <baudhikmadan2005@gmail.com>
2025-03-20 05:13:34 +0000

    numbers and maths in javascript

---
+++ number and maths/maths.js
@@ -0,0 +1,12 @@
+//console.log(Math);//description about maths
+//console.log(Math.abs(-44));//uses absolute inbuilt func in math
+
+//console.log(Math.round(43.5));
+// console.log(Math.ceil(43.5));//ceiling value
+// console.log(Math.floor(43.5));//floor value
+
+// console.log(Math.min(4,3,6,5,2));//min element
+// console.log(Math.max(4,3,6,5,2));//max element
+
+//math.random gives random number between 0 and 1
+
---
+++ number and maths/numbers.js
@@ -0,0 +1,15 @@
+const score=234
+//console.log(score);//output is only the value
+
+const balance = new Number(100)//output also give the typeof the variable with the values stored in it as well
+//console.log(balance);
+
+// console.log(balance.toString());//converts it into string
+// console.log(typeof balance);
+
+//console.log(balance.toFixed(3));//used to convert and int to flot by the number of digits fter decimal secified in the parenthesis
+
+const hun=100000000
+// console.log(hun.toLocaleString());//converts and add comma in usd standards
+// console.log(hun.toLocaleString('en-IN'));//converts in form of indian comma 
+
